0480d72ebd6fcabb7301989425586df1bd8ca10c
TerrificTable <omegagaming189@gmail.com>
2022-11-30 19:09:10 +0100

    Table footer

---
+++ src/main/kotlin/xyz/terrific/elements/Footer.kt
@@ -0,0 +1,10 @@
+package xyz.terrific.elements
+
+import org.jetbrains.annotations.Nullable
+import xyz.terrific.Tablealign
+
+class Footer(
+    @Nullable var data: String?,
+    val align: Tablealign = Tablealign.LEFT
+)
+
